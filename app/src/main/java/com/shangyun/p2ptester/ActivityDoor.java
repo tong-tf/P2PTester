afd9673419933a530d7eb13d130bb0ad62c1c698
tong <tj_wust@163.com>
2019-03-16 16:12:25 +0800

    ActivityDoor: 1. add unlock function 2. quit and send hangup after 45s without answer

--- app/src/main/java/com/shangyun/p2ptester/ActivityDoor.java
+++ app/src/main/java/com/shangyun/p2ptester/ActivityDoor.java
@@ -3,6 +3,8 @@ package com.shangyun.p2ptester;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +17,8 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class ActivityDoor extends Activity {
+
+    private final static int MSG_TIMEOUT = 0;
 
     @BindView(R.id.door_info)
     TextView doorInfo;
@@ -31,17 +35,32 @@ public class ActivityDoor extends Activity {
         did = intent.getStringExtra("peer_key");
         mHandle = intent.getIntExtra("handle", -1);
         doorInfo.setText(String.format(outsideCallInfo, did));
+        mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_TIMEOUT), 45 *1000); // 45S TIMEOUT
     }
 
-    @OnClick({R.id.door_accept, R.id.door_cancel})
+    @OnClick({R.id.door_accept, R.id.door_cancel,R.id.clooud_unlock})
     public void myClick(View view){
+        mHandler.removeMessages(MSG_TIMEOUT);
         switch (view.getId()){
-            case R.id.door_accept:
 
-                break;
-            case R.id.door_cancel:
+            case R.id.clooud_unlock:
                 if(mHandle >= 0){
                     doUnlockDoor();
+                }
+                finish();
+                break;
+
+            case R.id.door_accept:
+                Intent mIntent =  new Intent();
+                mIntent.setClass(ActivityDoor.this, Main3Activity.class);
+                startActivity(mIntent);
+                finish();
+                break;
+
+            case R.id.door_cancel:
+
+                if(mHandle >= 0){
+                    hangUp();
                 }
                 finish();
                 break;
@@ -49,9 +68,27 @@ public class ActivityDoor extends Activity {
     }
 
 
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case MSG_TIMEOUT:
+                    hangUp();
+                    finish();
+                    break;
+            }
+        }
+    };
+
     private void doUnlockDoor(){
         FeiflyJson.Builder builder = new FeiflyJson.Builder();
-        builder.setOperation("door unlock");
+        builder.setOperation("open door");
+        P2pnet.sendData(mHandle, builder.build());
+    }
+
+    private void hangUp(){
+        FeiflyJson.Builder builder = new FeiflyJson.Builder();
+        builder.setOperation("hang up");
         P2pnet.sendData(mHandle, builder.build());
     }
 }
